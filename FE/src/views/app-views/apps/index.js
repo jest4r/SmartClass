e2563ad60c3bc48deae10f967da48a108dfc30f7
Jester <jeremytruong0204@gmail.com>
2025-04-12 11:35:07 +0700

    test and build

--- FE/src/views/app-views/apps/index.js
+++ FE/src/views/app-views/apps/index.js
@@ -5,11 +5,6 @@ import Loading from 'components/shared-components/Loading';
 const Apps = ({ match }) => (
   <Suspense fallback={<Loading cover="content"/>}>
     <Switch>
-      <Route path={`${match.url}/mail`} component={lazy(() => import(`./mail`))} />
-      <Route path={`${match.url}/chat`} component={lazy(() => import(`./chat`))} />
-      <Route path={`${match.url}/calendar`} component={lazy(() => import(`./calendar`))} />
-      <Route path={`${match.url}/project`} component={lazy(() => import(`./project`))} />
-      <Route path={`${match.url}/ecommerce`} component={lazy(() => import(`./e-commerce`))} />
       <Route path={`${match.url}/classes`} component={lazy(() => import(`./classes`))} />
       <Route path={`${match.url}/students`} component={lazy(() => import(`./students`))} />
       <Route path={`${match.url}/add-class`} component={lazy(() => import(`./add-class`))} />
